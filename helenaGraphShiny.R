0fd0927637d5d968f7d7e561e4759abad9982659
helenamoore <jsk9gb@virginia.edu>
2023-12-03 19:39:07 -0500

    hwe

--- helenaGraphShiny.R
+++ helenaGraphShiny.R
@@ -9,15 +9,19 @@ ui = fluidPage(
   sidebarLayout(
     sidebarPanel(
       
-      selectInput("select", "Select:", c("Continent", "Country"), selected = "Continent"), 
+      radioButtons("select", "Select:", c("Continent", "Country"), selected = "Continent"),
       
       conditionalPanel(
         condition = "input.select == 'Country'",
         selectInput(inputId = "y",
                     label = "Y-Axis:",
                     choices = c("South America", "Oceania", "North America", "Europe","Asia","Africa"),selected = "North America"
-                    )
+        )
       ),
+      
+     sliderInput("range", "Year Range:", min = 2015, max = 2019, value = c(2015,2019)),
+      
+      
   
       
     ), #sidebar panel
@@ -29,7 +33,8 @@ ui = fluidPage(
 server = function(input,output){
   
   overall_dataset <- reactive({
-    Cleaned_Combined_Dataset_v2 %>%
+    version3 %>%
+      filter(Year >= input$range[1] & Year <= input$range[2]) %>% 
       group_by(Continent, Year) %>%
       summarize(avgHappiness = mean(Happiness.Score, na.rm = TRUE)) %>%
       ungroup()
@@ -37,8 +42,8 @@ server = function(input,output){
   
   selected_continent_dataset <- reactive({
     if (input$select == "Country") {
-      Cleaned_Combined_Dataset_v2 %>%
-        filter(Continent == input$y) %>%
+     version3 %>%
+        filter(Continent == input$y, Year >= input$range[1] & Year <= input$range[2]) %>%
         group_by(Country, Year) %>%
         summarize(avgHappiness = mean(Happiness.Score, na.rm = TRUE)) %>%
         ungroup()
@@ -50,10 +55,10 @@ server = function(input,output){
   #render heat map 
   output$dynamicHeatmap <- renderPlot({
     if(input$select == "Continent") {
-    ggplot(selected_continent_dataset(), aes(x=Year, y=Continent, fill=avgHappiness))+geom_raster()+scale_fill_viridis_c(option="viridis")
+    ggplot(selected_continent_dataset(), aes(x=Year, y=Continent, fill=avgHappiness))+geom_raster()+scale_fill_viridis_c(option="magma")
     }
     else if (input$select == "Country"){
-      ggplot(selected_continent_dataset(), aes(x=Year, y=Country, fill=avgHappiness))+geom_raster()+scale_fill_viridis_c(option="viridis")
+      ggplot(selected_continent_dataset(), aes(x=Year, y=Country, fill=avgHappiness))+geom_raster()+scale_fill_viridis_c(option="magma")
     }
     
   })
